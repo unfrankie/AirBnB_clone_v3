6c6d2c026a7953b6f4bd115dffe9e64e9dd6fad9
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-26 15:20:32 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,7 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_db(exception):
   """ teardown """
-  storage.close()
+     storage.close()
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
