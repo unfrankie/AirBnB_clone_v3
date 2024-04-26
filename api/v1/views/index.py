aaacdf8371b21eda5767486e866ce82690f37e4a
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-26 15:26:16 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,12 @@
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def api_status():
     """api status"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'])
 def obj_count():
