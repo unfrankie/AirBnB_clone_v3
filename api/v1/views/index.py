e7f69fac8253337141f2c08edadf0a185105bd34
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-26 15:13:18 +0100

    Create index.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" index """
+
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status', methods=['GET'])
+def api_status():
+    return jsonify({"status": "OK"})
