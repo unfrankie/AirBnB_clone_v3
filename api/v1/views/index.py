ceb067b9d3df922240e9cd58e44e107989c4e9ff
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-26 21:32:18 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,23 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def api_status():
     """api status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def obj_count():
+    """Endpoint that retrieves the number of each objects by type"""
+    classes = {
+        "Amenity": storage.count("Amenity"),
+        "City": storage.count("City"),
+        "Place": storage.count("Place"),
+        "Review": storage.count("Review"),
+        "State": storage.count("State"),
+        "User": storage.count("User")
+    }
+    return jsonify(classes)
