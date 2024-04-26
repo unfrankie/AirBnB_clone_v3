b9f062f3c33045955fb0f048fb4bebff952375d2
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-26 15:16:55 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,4 +6,18 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def api_status():
+    """api status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def obj_count():
+    """Endpoint that retrieves the number of each objects by type"""
+    classes = {
+        "Amenity": storage.count("Amenity"),
+        "City": storage.count("City"),
+        "Place": storage.count("Place"),
+        "Review": storage.count("Review"),
+        "State": storage.count("State"),
+        "User": storage.count("User")
+    }
+    return jsonify(classes)
