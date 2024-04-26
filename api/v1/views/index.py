d9feb9123918cda0e37b5f1ec9c5e584e2412ab5
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-26 21:42:35 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 
 from flask import jsonify
 from api.v1.views import app_views
-from models import *
+from models import storage, Amenity, City, Place, Review, State, User
 
 
 @app_views.route('/status')
