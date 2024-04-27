4834e32293db1413f20cd8eefb53f98d47960d64
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-27 20:16:35 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,8 +7,8 @@ import os
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
