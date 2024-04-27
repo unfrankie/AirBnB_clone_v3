1b8754661fd7eb59c570ac9a176048776c75c151
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-27 19:50:36 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,6 +16,11 @@ def teardown_db(exception):
     """" Teardown Database"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """ Not Found 404 """
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
