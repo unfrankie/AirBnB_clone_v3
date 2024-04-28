3201ad843016705bcb541a9b1a682bb4ea5ff786
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-28 13:40:53 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -6,11 +6,12 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+storage_t = getenv("HBNB_TYPE_STORAGE")
 
 
 class Amenity(BaseModel, Base):
     """Representation of Amenity """
-    if models.storage_t == 'db':
+    if storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
