efa867bf9d04640ae71f9349476539b7cd8208ca
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-28 18:23:54 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """ index """
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
