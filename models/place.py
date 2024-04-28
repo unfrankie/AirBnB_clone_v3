0f903ba0862fa2fddaaff44cde62b0be862065f4
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-28 13:42:49 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -6,8 +6,10 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+storage_t = getenv("HBNB_TYPE_STORAGE")
 
-if models.storage_t == 'db':
+
+if storage_t == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id', onupdate='CASCADE',
@@ -21,7 +23,7 @@ if models.storage_t == 'db':
 
 class Place(BaseModel, Base):
     """Representation of Place """
-    if models.storage_t == 'db':
+    if storage_t == 'db':
         __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -54,7 +56,7 @@ class Place(BaseModel, Base):
         """initializes Place"""
         super().__init__(*args, **kwargs)
 
-    if models.storage_t != 'db':
+    if storage_t != 'db':
         @property
         def reviews(self):
             """getter attribute returns the list of Review instances"""
