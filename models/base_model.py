839f808f264af6ce4ec202562ee4af760b12ce92
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-28 14:58:06 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -60,7 +60,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, **kwargs):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
