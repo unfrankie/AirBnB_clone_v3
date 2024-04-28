3334fd071e78301566ea10b050074cb2ebb0d4ae
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-28 18:05:02 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,21 +4,17 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """api status"""
-    return jsonify({"status": "OK"})
+    out = jsonify({"status": "OK"})
+    out.status_code = 200
+    return out
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def obj_count():
     """Endpoint that retrieves the number of each objects by type"""
     classes = {
@@ -29,4 +25,6 @@ def obj_count():
         "State": storage.count(State),
         "User": storage.count(User)
     }
-    return jsonify(classes)
+    out = jsonify(classes)
+    out.status_code = 200
+    return out
